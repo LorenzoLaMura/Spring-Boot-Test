ede722fd7ef1f03cfe9c7cb401839d65d98ed272
LorenzoLaMura <lorenzo.la-mura@alumni.univ-avignon.fr>
2023-04-03 15:08:19 +0200

    Add Documentation Swagger

--- src/main/java/com/springboot/api/ApiApplication.java
+++ src/main/java/com/springboot/api/ApiApplication.java
@@ -1,8 +1,17 @@
 package com.springboot.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "SpringBoot API",
+				version = "1.0",
+				description = "SpringBoot API Test documentation"
+		)
+)
 @SpringBootApplication
 public class ApiApplication {
 
--- src/main/java/com/springboot/api/controller/EmployeeController.java
+++ src/main/java/com/springboot/api/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.springboot.api.controller;
 
 import com.springboot.api.model.Employee;
 import com.springboot.api.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,8 @@ public class EmployeeController {
      * @param employee An object employee
      * @return The employee object saved
      */
+    @Operation(summary = "Add a new employee")
+    @ApiResponse(responseCode = "201", description = "Employee created")
     @PostMapping("/add/employee")
     public Employee createEmployee(@RequestBody Employee employee) {
         return  employeeService.saveEmployee(employee);
@@ -27,6 +31,8 @@ public class EmployeeController {
      * Read - Get all employees
      * @return - An Iterable object of Employees fulfilled
      */
+    @Operation(summary = "Get all the employees")
+    @ApiResponse(responseCode = "200", description = "Employees returned")
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
@@ -37,6 +43,8 @@ public class EmployeeController {
      * @param id The id of the employee
      * @return An employee object fulfilled
      */
+    @Operation(summary = "Get an employee")
+    @ApiResponse(responseCode = "200", description = "Employee returned")
     @GetMapping("/employee/{id}")
     public Employee getEmployee(@PathVariable("id") final Long id) {
         Optional<Employee> employee = employeeService.getEmployee(id);
@@ -47,11 +55,15 @@ public class EmployeeController {
      * Delete - Delete an employee
      * @param id - The id of the employee to delete
      */
+    @Operation(summary = "Remove an employee")
+    @ApiResponse(responseCode = "200", description = "Employee removed")
     @DeleteMapping("remove/employee/{id}")
     public void deleteEmployee(@PathVariable("id") final Long id) {
         employeeService.deleteEmployee(id);
     }
 
+    @Operation(summary = "Update an employee")
+    @ApiResponse(responseCode = "200", description = "Employee updated")
     @PutMapping("update/employee/{id}")
     public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
         Optional<Employee> e = employeeService.getEmployee(id);
