54cf7c4a4238d4a4b7e9f67cbb9a657da25ad897
LorenzoLaMura <lorenzo.la-mura@alumni.univ-avignon.fr>
2023-04-03 11:41:17 +0200

    Add Employees classes

---
+++ src/main/java/com/springboot/api/controller/EmployeeController.java
@@ -0,0 +1,83 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.model.Employee;
+import com.springboot.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * Create - Add a new employee
+     * @param employee An object employee
+     * @return The employee object saved
+     */
+    @PostMapping("/add/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return  employeeService.saveEmployee(employee);
+    }
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employees fulfilled
+     */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    /**
+     * Read - Get one employee
+     * @param id The id of the employee
+     * @return An employee object fulfilled
+     */
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") final Long id) {
+        Optional<Employee> employee = employeeService.getEmployee(id);
+        return employee.orElse(null); // If employee is found it returns it, or null is returned
+    }
+
+    /**
+     * Delete - Delete an employee
+     * @param id - The id of the employee to delete
+     */
+    @DeleteMapping("remove/employee/{id}")
+    public void deleteEmployee(@PathVariable("id") final Long id) {
+        employeeService.deleteEmployee(id);
+    }
+
+    @PutMapping("update/employee/{id}")
+    public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
+        Optional<Employee> e = employeeService.getEmployee(id);
+        if(e.isPresent()) {
+            Employee currentEmployee = e.get();
+
+            String firstName = currentEmployee.getFirstName();
+            if (firstName != null) {
+                currentEmployee.setFirstName(firstName);
+            }
+            String lastName = currentEmployee.getLast_name();
+            if (lastName != null) {
+                currentEmployee.setLast_name(lastName);
+            }
+            String mail = currentEmployee.getMail();
+            if (mail != null) {
+                currentEmployee.setMail(mail);
+            }
+            String password = currentEmployee.getPassword();
+            if (password != null) {
+                currentEmployee.setPassword(password);
+            }
+
+            return currentEmployee;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/springboot/api/model/Employee.java
@@ -0,0 +1,25 @@
+package com.springboot.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    private String mail;
+
+    private String password;
+
+}
